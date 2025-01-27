b86baf9690006c4b95a75581d10d391ebadc72de
Vinicius Miguel <viniciusmiguel.dev@gmail.com>
2025-01-27 18:25:24 -0300

    Projeto Filmes API (V0.1.0)

---
+++ filmes/src/pages/Filme/index.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+import "./filme-info.css";
+
+import api from "../../services/api";
+
+function Filme() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "66ba5288f6e4946642782c650c7d64d1",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          navigate("/", { replace: true });
+          return;
+        });
+    }
+    loadFilme();
+
+    return () => {
+      // alert("Componente off");
+    };
+  }, [navigate, id]);
+
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@primeflix");
+
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some((filmesSalvos) => filmesSalvos.id == filme.id);
+
+    if (hasFilme) {
+      alert("Esse filme já está na lista");
+      return;
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+  }
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando Filme...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+      ></img>
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button onClick={salvarFilme}>Salvar</button>
+        <button>
+          <a
+            target="blank"
+            rel="external"
+            href={`https://youtube.com/results?search_query=${filme.title} trailer`}
+          >
+            Trailer
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Filme;
---
+++ filmes/src/routes.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Filme from "./pages/Filme";
+
+import Erro from "./pages/Erro";
+
+import Header from "./components/Header/index";
+
+function RoutesApp() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/filme/:id" element={<Filme />} />
+
+        <Route path="*" element={<Erro />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default RoutesApp;
